c59c26c480cd394b619f7ecc2cb9b42f89796aed
Davi Monteiro <davi321522@gmail.com>
2024-12-10 15:55:56 -0300

    TestarComCodigoDoSace

--- WeatherAPI_WEB/obj/Debug/net8.0/WeatherAPI_WEB.AssemblyInfo.cs
+++ WeatherAPI_WEB/obj/Debug/net8.0/WeatherAPI_WEB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherAPI_WEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab73b5364508a1e45660bda9d91194b4988cf162")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherAPI_WEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherAPI_WEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
